5cd3d874b39a95f27af9a3be4dae6a2b1f5fb75a
Khayal_Dev <82506368+Khayal-Dev@users.noreply.github.com>
2023-06-23 19:16:20 +0300

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { createGetInitialProps } from '@mantine/next'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class _Document extends Document {
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
